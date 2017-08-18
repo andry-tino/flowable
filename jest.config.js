af8ea67ec5184d0962bbf499207da1e41abe6e3e
Andrea <andry.tino@gmail.com>
2017-08-18 23:16:35 +0200

    Introducing babel

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     verbose: true,
+    testRegex: "(/test/brws.*|(\\.|/)(test|spec))\\.js$"
 };
--- test/brws-box/box.test.js
+++ test/brws-box/box.test.js
@@ -3,6 +3,8 @@
  * Andrea Tino - 2017
  */
 
+import * as relation from "../../src/box";
+
 describe("basic", function() {
     it("Test some basic shit", function() {
         expect(0).toBe(0);
