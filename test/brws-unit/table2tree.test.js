7ae26ca499f218d2dcec9ac0101dda6d7053be87
Andrea <andry.tino@gmail.com>
2017-08-20 15:15:19 +0200

    Tests for table to tree conversion

--- test/brws-unit/table2tree.test.js
+++ test/brws-unit/table2tree.test.js
@@ -36,6 +36,35 @@ describe("TestSuite for conversion from table to tree", function() {
         testTreeTraverse(root, "box-000000;box-000001;box-000002;box-000003;", 4);
     });
 
+    it("Binary tree with 2 levels", function() {
+        let table = new tables.RelationsTable();
+
+        let box0 = new boxes.Box("box-000000");
+        let box11 = new boxes.Box("box-000011");
+        let box12 = new boxes.Box("box-000012");
+        let box21 = new boxes.Box("box-000021");
+        let box22 = new boxes.Box("box-000022");
+        let box23 = new boxes.Box("box-000023");
+        let box24 = new boxes.Box("box-000024");
+
+        // Adding relations
+        table.add(new relations.Relation(box0, box11));
+        table.add(new relations.Relation(box0, box12));
+        table.add(new relations.Relation(box11, box21));
+        table.add(new relations.Relation(box11, box22));
+        table.add(new relations.Relation(box12, box23));
+        table.add(new relations.Relation(box12, box24));
+
+        // Convert
+        var root = new treeTableConversions.Table2Tree(table).convert();
+
+        // Root should be correctly identified
+        expect(root).toBeTruthy();
+        expect(root.id).toEqual("box-000000");
+
+        testTreeTraverse(root, "box-000000;box-000011;box-000021;box-000022;box-000012;box-000023;box-000024;", 7);
+    });
+
 });
 
 function testTreeTraverse(root, expectedOutput, expectedCount) {
